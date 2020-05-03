0ecf64e7555227e5f04db4adfd06818dba37196e
Your Name <samanbatool08@gmail.com>
2020-05-03 15:50:27 -0400

    avoid sql injection attack

--- database-app.py
+++ database-app.py
@@ -11,5 +11,5 @@ def index():
     q = request.args.get("q")
     # can cause SQL injection attack
     # rows = db.execute(f"SELECT * FROM registrants WHERE name = '{q}' ")
-    rows = db.execute(f"SELECT * FROM registrants WHERE name = :name", name='{q}')
+    rows = db.execute("SELECT * FROM registrants WHERE name = :name", name='{q}')
     return render_template("index.html", rows=rows)
